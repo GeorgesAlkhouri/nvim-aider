e5b20eb2570ebb1e8764a712150970fc86dfefad
Georges Alkhouri <georges.alkhouri@gmail.com>
2025-01-05 21:58:51 +0100

    feat(devenv): :sparkles: add lua & tests
    Add a separate lua package in devenv.nix to replace luajit usage and
    update 'enterTest' to clean up test directories gracefully. Also add a
    new test for nvim_aider that calls setup without arguments.

--- devenv.nix
+++ devenv.nix
@@ -9,26 +9,16 @@ let
   unstable = import inputs.nixpkgs-unstable { system = pkgs.stdenv.system; };
 in
 {
-
-  env.LUA_PATH = "$LUA_PATH;?.lua";
   packages = with unstable; [
     gnumake
     git
-    lua-language-server
-    stylua
-    lua
-    luajitPackages.luafilesystem
-    luajitPackages.luarocks
   ];
-  languages.lua.enable = true;
-  languages.lua.package = unstable.lua;
   languages.python.enable = true;
   languages.python.uv.enable = true;
   languages.python.venv.enable = true;
   languages.python.venv.requirements = ''
     aider-chat
   '';
-
   enterTest = ''
     make test
   '';
