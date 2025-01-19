18d376fa2f583d5ac7686b9c9eaa5d6f15cc1cad
Boris Churzin <boris@nilus.com>
2025-01-19 13:00:51 +0200

    cr fix(*): absolute path for read-only command

--- lua/nvim_aider/init.lua
+++ lua/nvim_aider/init.lua
@@ -98,12 +98,11 @@ function M.setup(opts)
   end, {})
 
   vim.api.nvim_create_user_command("AiderQuickReadOnlyFile", function()
-    local absolute_path = utils.get_absolute_path()
-    if absolute_path == nil then
+    local filepath = utils.get_absolute_path()
+    if filepath == nil then
       vim.notify("No valid file in current buffer", vim.log.levels.INFO)
     else
-      local relative_filepath = vim.fn.fnamemodify(absolute_path, ":.")
-      M.terminal.command(commands["read-only"].value, relative_filepath)
+      M.terminal.command(commands["read-only"].value, filepath)
     end
   end, {})
 
